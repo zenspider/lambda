592a9f36ef8dccae1f322db238bf7242aa3481ff
Ryan Davis <ryand-ruby@zenspider.com>
2015-11-24 21:15:58 -0800

    YAY! Got the thing working. Special thanks to endobson in #racket.

--- semantics.rkt
+++ semantics.rkt
@@ -8,9 +8,9 @@
 
 (define-syntax-rule (prog e ...)         (begin e ...))
 (define-syntax-rule (expr e)             e)
-(define-syntax-rule (func _ name _ body) (list 'lambda (list name) body))
-(define-syntax-rule (appl _ lexp aexp _) (list lexp aexp))
-(define-syntax-rule (name id)            'id)
+(define-syntax-rule (func _ (_ n) _ b)   (lambda (n) b))
+(define-syntax-rule (appl _ lexp aexp _) (lexp aexp))
+(define-syntax-rule (name id)            id)
 (define-syntax-rule (body e)             e)
 (define-syntax-rule (lexp e)             e)
 (define-syntax-rule (aexp e)             e)
